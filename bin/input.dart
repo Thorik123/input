bacbc9b267c79b7da143b680f015adb989f8f82f
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 18:13:33 +0700

    first commit

---
+++ bin/input.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  String input = stdin.readLineSync();
+  String alamat = stdin.readLineSync();
+
+  print('Kampus saya => ' + input);
+  print('Alamat saya =>' + alamat);
+}
